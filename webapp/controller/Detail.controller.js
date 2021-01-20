b6fca564406f82e77143b77b9e18b00ce3035f1d
DanielVasilev98 <kyn_aguero_10@abv.bg>
2021-01-20 16:12:53 +0000

    Delete functionality added, plus Edit view.

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
 	"./BaseController",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-	"sap/m/library"
-], function (BaseController, JSONModel, formatter, mobileLibrary) {
+	"sap/m/library",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
+	"sap/ui/Device"
+], function (BaseController, JSONModel, formatter, mobileLibrary, MessageBox, MessageToast, Device) {
 	"use strict";
 
 	// shortcut for sap.m.URLHelper
@@ -17,13 +20,13 @@ sap.ui.define([
 		/* lifecycle methods                                           */
 		/* =========================================================== */
 
-		onInit : function () {
+		onInit: function () {
 			// Model used to manipulate control states. The chosen values make sure,
 			// detail page is busy indication immediately so there is no break in
 			// between the busy indication for loading the view's meta data
 			var oViewModel = new JSONModel({
-				busy : false,
-				delay : 0
+				busy: false,
+				delay: 0
 			});
 
 			this.getRouter().getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
@@ -41,7 +44,7 @@ sap.ui.define([
 		 * Event handler when the share by E-Mail button has been clicked
 		 * @public
 		 */
-		onSendEmailPress : function () {
+		onSendEmailPress: function () {
 			var oViewModel = this.getModel("detailView");
 
 			URLHelper.triggerEmail(
@@ -55,21 +58,20 @@ sap.ui.define([
 		 * Event handler when the share in JAM button has been clicked
 		 * @public
 		 */
-		onShareInJamPress : function () {
+		onShareInJamPress: function () {
 			var oViewModel = this.getModel("detailView"),
 				oShareDialog = sap.ui.getCore().createComponent({
-					name : "sap.collaboration.components.fiori.sharing.dialog",
-					settings : {
-						object :{
-							id : location.href,
-							share : oViewModel.getProperty("/shareOnJamTitle")
+					name: "sap.collaboration.components.fiori.sharing.dialog",
+					settings: {
+						object: {
+							id: location.href,
+							share: oViewModel.getProperty("/shareOnJamTitle")
 						}
 					}
 				});
 
 			oShareDialog.open();
 		},
-
 
 		/* =========================================================== */
 		/* begin: internal methods                                     */
@@ -81,14 +83,14 @@ sap.ui.define([
 		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
 		 * @private
 		 */
-		_onObjectMatched : function (oEvent) {
-			var personalId =  oEvent.getParameter("arguments").PersonalID;
-			var visaId =  oEvent.getParameter("arguments").VisaID;
+		_onObjectMatched: function (oEvent) {
+			var personalId = oEvent.getParameter("arguments").PersonalID;
+			var visaId = oEvent.getParameter("arguments").VisaID;
 			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
-			this.getModel().metadataLoaded().then( function() {
+			this.getModel().metadataLoaded().then(function () {
 				var sObjectPath = this.getModel().createKey("VisaRequestSet", {
-					PersonalID :  personalId,
-					VisaID : visaId
+					PersonalID: personalId,
+					VisaID: visaId
 				});
 				this._bindView("/" + sObjectPath);
 			}.bind(this));
@@ -101,7 +103,7 @@ sap.ui.define([
 		 * @param {string} sObjectPath path to the object to be bound to the view.
 		 * @private
 		 */
-		_bindView : function (sObjectPath) {
+		_bindView: function (sObjectPath) {
 			// Set busy indicator during view binding
 			var oViewModel = this.getModel("detailView");
 
@@ -109,10 +111,10 @@ sap.ui.define([
 			oViewModel.setProperty("/busy", false);
 
 			this.getView().bindElement({
-				path : sObjectPath,
+				path: sObjectPath,
 				events: {
-					change : this._onBindingChange.bind(this),
-					dataRequested : function () {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function () {
 						oViewModel.setProperty("/busy", true);
 					},
 					dataReceived: function () {
@@ -122,7 +124,7 @@ sap.ui.define([
 			});
 		},
 
-		_onBindingChange : function () {
+		_onBindingChange: function () {
 			var oView = this.getView(),
 				oElementBinding = oView.getElementBinding();
 
@@ -144,7 +146,7 @@ sap.ui.define([
 
 			this.getOwnerComponent().oListSelector.selectAListItem(sPath);
 
-			oViewModel.setProperty("/saveAsTileTitle",oResourceBundle.getText("shareSaveTileAppTitle", [sObjectName]));
+			oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("shareSaveTileAppTitle", [sObjectName]));
 			oViewModel.setProperty("/shareOnJamTitle", sObjectName);
 			oViewModel.setProperty("/shareSendEmailSubject",
 				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
@@ -152,7 +154,7 @@ sap.ui.define([
 				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
 		},
 
-		_onMetadataLoaded : function () {
+		_onMetadataLoaded: function () {
 			// Store original busy indicator delay for the detail view
 			var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
 				oViewModel = this.getModel("detailView");
@@ -189,9 +191,93 @@ sap.ui.define([
 				this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
 			} else {
 				// reset to previous layout
-				this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
+				this.getModel("appView").setProperty("/layout", this.getModel("appView").getProperty("/previousLayout"));
 			}
-		}
+		},
+		getMessage: function (message) {
+			var oModel = this.getView().getModel("i18n");
+			var resourceBundle = oModel.getResourceBundle();
+			return resourceBundle.getText(message);
+		},
+		onDelete: function () {
+			var oView = this.getView(),
+				oDataModel = oView.getModel();
+			var that = this;
+			MessageBox.show(this.getMessage("areYouSureYouWantToWithdraw"), {
+				icon: sap.m.MessageBox.Icon.QUESTION,
+				title: "",
+				actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+				styleClass: this.getOwnerComponent().getContentDensityClass(),
+				onClose: function (oAction) {
+					if (oAction === sap.m.MessageBox.Action.YES) {
+
+						var oParams = {};
+						oParams.method = "POST";
+						oParams.success = function (oData, oResponse) {
+							sap.ui.core.BusyIndicator.hide();
+							MessageToast.show(this.getMessage("successfulWithdrawal"));
+							// this._bindView(oView.getBindingContext().sPath);
+							oDataModel.refresh(true, true);
+							// models.createEmployeeInitialsModel(this.getOwnerComponent());
+							that.setFirstItem();
+						}.bind(this);
+
+						oParams.error = function (oError) {
+							sap.ui.core.BusyIndicator.hide();
+							// that.showError(that.getErrorMessage(oError));
+						}.bind(this);
+
+						oParams.async = true;
+						sap.ui.core.BusyIndicator.show(0);
+						oDataModel.remove(oView.getBindingContext().sPath, oParams);
+					}
+				}.bind(this)
+			});
+			// var oModel = this.getView().getModel();
+			// var personalId = this.getView().byId("personalId").getText();
+			// var visaId = this.getView().byId("visaId").getText();
+			// var delurl = this.getView().getBindingContext().getPath();
+			// MessageBox.confirm("Are you sure that you want to delete this visa?", {
+			// 	onClose: function (sButton) {
+			// 		if (sButton === MessageBox.Action.OK) {
+			// 			oModel.remove(delurl);
+			// 			oModel.refresh();
+			// 		}
+			// 	}
+			// });
+
+		},
+		setFirstItem: function() {
+            var bReplace = !Device.system.phone;
+            if (this.getList().getItems().length > 0) {
+                var firstItem = this.getList().getItems()[0];
+                this.getList().setSelectedItem(firstItem, true);
+                this.getList().setSelectedItem(firstItem);
+                this.getRouter().navTo("detail", {
+                    PersonalID: firstItem.getBindingContext().sPath.split("'")[1],
+                    VisaID: firstItem.getBindingContext().sPath.split("'")[3]
+                }, bReplace);
+            }
+ 
+        },
+        getList: function() {
+            var sComponentId = sap.ui.core.Component.getOwnerIdFor(this.oView),
+                oComponent = sap.ui.component(sComponentId);
+            var master = oComponent.getRouter().getView("home.kpmg.VisaTask.view.Master");
+            var list = master.byId("list");
+            return list;
+        },
+        
+        onEdit: function() {
+        	var bReplace = !Device.system.phone;
+			// set the layout property of FCL control to show two columns
+			var oView = this.getView();
+			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+			this.getRouter().navTo("Edit", {
+				PersonalID : oView.getBindingContext().getProperty("PersonalID"),
+				VisaID : oView.getBindingContext().getProperty("VisaID")
+			}, bReplace);
+        }
 	});
 
 });
