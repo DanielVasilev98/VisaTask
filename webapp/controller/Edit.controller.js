2698f3f7a6b0ba2c2d3f1183fd6a23966f6f8098
DanielVasilev98 <kyn_aguero_10@abv.bg>
2021-01-22 10:13:56 +0000

    Edit view and Edit controller are now finished!

--- webapp/controller/Edit.controller.js
+++ webapp/controller/Edit.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"./BaseController",
 	"sap/ui/core/routing/History",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, History, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast"
+], function (Controller, History, JSONModel, MessageBox, MessageToast) {
 	"use strict";
 
 	return Controller.extend("home.kpmg.VisaTask.controller.Edit", {
@@ -26,7 +28,7 @@ sap.ui.define([
 			// var jsModel = new JSONModel(jsData);
 			// this.getView().setModel(jsModel, "editView");
 			// this.getView().getModel("editView").updateBindings(true);
-			debugger;
+
 		},
 		_onObjectMatched: function (oEvent) {
 			var personalId = oEvent.getParameter("arguments").PersonalID;
@@ -74,21 +76,10 @@ sap.ui.define([
 				return;
 			}
 
-			var sPath = oElementBinding.getPath(),
-				oResourceBundle = this.getResourceBundle(),
-				oObject = oView.getModel().getObject(sPath),
-				sObjectId = oObject.PersonalID,
-				sObjectName = oObject.Manager,
-				oViewModel = this.getModel("detailView");
+			var sPath = oElementBinding.getPath();
 
 			this.getOwnerComponent().oListSelector.selectAListItem(sPath);
 
-			oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("shareSaveTileAppTitle", [sObjectName]));
-			oViewModel.setProperty("/shareOnJamTitle", sObjectName);
-			oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-			oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
 		},
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -115,7 +106,7 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-		onNavBack: function () {
+		onBack: function () {
 			var sPreviousHash = History.getInstance().getPreviousHash(),
 				oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
 
@@ -124,37 +115,112 @@ sap.ui.define([
 				history.go(-1);
 			}
 		},
-		
+		getMessage: function (message) {
+			var oModel = this.getView().getModel("i18n");
+			var resourceBundle = oModel.getResourceBundle();
+			return resourceBundle.getText(message);
+		},
+		showError: function (text) {
+			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			MessageBox.error(
+				text, {
+					styleClass: bCompact ? "sapUiSizeCompact" : ""
+				}
+			);
+		},
+		getErrorMessage: function (response) {
+			var message = jQuery.parseJSON(response.responseText).error.message.value;
+			return message;
+		},
+
 		onSave: function () {
-			debugger;
-			// var oModel = this.getView().getModel();
 
-			// var model = this.getView().getModel();
+			// var oModel = this.getView().getBindingContext().getObject();
+			// var test = this.getView().getModel("updateView");
 
-			// var data = model.getData();
-			// debugger;
-			// if (data.Paid === 0) {
-			// 	data.Paid = "X";
-			// } else {
-			// 	data.Paid = "";
-			// }
+			var visaType = this.getView().byId("VisaTypeInput").getValue();
+			var reqReason = this.getView().byId("productInput").getValue();
+			var paid = this.getView().byId("paidBtn").getSelectedIndex();
+			var payment = this.getView().byId("amount").getValue();
+			var timePeriod = this.getView().byId("timePeriod").getSelectedKey();
+			var depTime = this.getView().byId("DP1").getValue();
+			var retTime = this.getView().byId("DP2").getValue();
 
-			// var dDate = data.DepTime;
-			// var rDate = data.ReturnTime;
-			// var depDate = new Date(dDate);
-			// var retDate = new Date(rDate);
-			// var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({
-			// 	pattern: "yyyy-MM-ddTHH:mm:ss"
+			var jsData = {
+				ReqReason: reqReason,
+				Paid: paid,
+				Status: "1",
+				Payment: payment,
+				TimePeriod: timePeriod,
+				DepTime: depTime,
+				ReturnTime: retTime
+			};
+
+			if (visaType === "Single") {
+				jsData.VisaType = "S";
+			} else {
+				jsData.VisaType = "M";
+			}
+
+			if (paid === 0) {
+				jsData.Paid = "X";
+			} else {
+				jsData.Paid = "";
+			}
+
+			var dDate = jsData.DepTime;
+			var rDate = jsData.ReturnTime;
+			var depDate = new Date(dDate);
+			var retDate = new Date(rDate);
+			var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({
+				pattern: "yyyy-MM-ddTHH:mm:ss"
+			});
+			var dDateFormatted = dateFormat.format(depDate);
+			var rDateFormatted = dateFormat.format(retDate);
+			jsData.DepTime = dDateFormatted;
+			jsData.ReturnTime = rDateFormatted;
+
+			var updateUrl = this.getView().getBindingContext().getPath();
+
+			var oModel = this.getView().getModel();
+			// oModel.update(updateUrl, jsData, {
+			// 	method: "PUT"
 			// });
-			// var dDateFormatted = dateFormat.format(depDate);
-			// var rDateFormatted = dateFormat.format(retDate);
-			// data.DepTime = dDateFormatted;
-			// data.ReturnTime = rDateFormatted;
-   //         var delurl = this.getView().getBindingContext().getPath();
-     
-			// jQuery.sap.require("sap.ui.commons.MessageBox");
 
-			// oModel.update('/VisaRequestSet', data, {method: "PUT"});
+			var that = this;
+			MessageBox.show(this.getMessage("updateVisaMessage"), {
+				icon: sap.m.MessageBox.Icon.QUESTION,
+				title: "",
+				actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+				styleClass: this.getOwnerComponent().getContentDensityClass(),
+				onClose: function (oAction) {
+					if (oAction === sap.m.MessageBox.Action.YES) {
+
+						var oParams = {};
+						oParams.method = "PUT";
+						oParams.success = function (oData, oResponse) {
+							sap.ui.core.BusyIndicator.hide();
+							MessageToast.show(this.getMessage("successfulWithdrawal"));
+							// this._bindView(oView.getBindingContext().sPath);
+							oModel.refresh(true, true);
+							// models.createEmployeeInitialsModel(this.getOwnerComponent());
+						}.bind(this);
+
+						oParams.error = function (oError) {
+							sap.ui.core.BusyIndicator.hide();
+							that.showError(that.getErrorMessage(oError));
+						}.bind(this);
+
+						oParams.async = true;
+						sap.ui.core.BusyIndicator.show(0);
+						oModel.update(updateUrl, jsData, oParams);
+					}
+					this.getRouter().navTo("detail", {
+						PersonalID: this.getView().getBindingContext().getProperty("PersonalID"),
+						VisaID: this.getView().getBindingContext().getProperty("VisaID")
+					});
+				}.bind(this)
+			});
 		}
 	});
 
